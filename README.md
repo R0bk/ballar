6a62d3896b70b62a88f76638fcb518168dcb7eea
Rob <rob.kopel@gmail.com>
2018-04-21 11:07:46 +1000

    App now fully works

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ballar
+
+export FLASK_APP=app.py
+
+flask run --port=80 --host=0.0.0.0
--- app.py
+++ app.py
@@ -5,6 +5,5 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello/<name>')
-def hello(name=None):
+def ballar():
     return render_template('ballar.html')
